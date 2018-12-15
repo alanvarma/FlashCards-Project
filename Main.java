39443484cd7de2b167ddc4746b0ee88f79fe8edb
aalrehn <aalrehn@luc.edu>
2018-12-15 01:00:43 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+import view.MainGUI;
+
+/**
+ * Main class contains main method
+ */
+public class Main {
+
+	/**
+	 * main method to start Java application
+	 */
+	public static void main(String[] args) {
+		
+		MainGUI GUI = new MainGUI();
+		GUI.setVisible(true);
+
+	}
+
+}
